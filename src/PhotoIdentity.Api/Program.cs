46606e1e3f3256f0963caa292b6b7651436bdcd8
Erik Wahlström <54457926+erikwasa@users.noreply.github.com>
2026-08-10 01:12:49 +0200

    Wire unattended archive advancement

--- src/PhotoIdentity.Api/Program.cs
+++ src/PhotoIdentity.Api/Program.cs
@@ -66,6 +66,7 @@ public partial class Program
         builder.Services.AddSingleton<SqliteArchiveSourceVerificationStateRepository>();
         builder.Services.AddSingleton<SqliteArchiveAvailabilityRepository>();
         builder.Services.AddSingleton<SqliteArchiveStorageRepository>();
+        builder.Services.AddSingleton<SqliteArchiveAdvancementRepository>();
         builder.Services.AddSingleton<ReviewCropFileResolver>();
         builder.Services.AddSingleton<DetectorRolloutCropFileResolver>();
         builder.Services.AddSingleton<CollectionPhotoFileResolver>();
@@ -78,6 +79,7 @@ public partial class Program
         builder.Services.AddSingleton<IArchiveStorageProbe, DriveArchiveStorageProbe>();
         builder.Services.AddSingleton<OpenCvThumbnailRenderer>();
         builder.Services.AddSingleton(TimeProvider.System);
+        builder.Services.AddHostedService<ArchiveAdvancementHostedService>();
         builder.Services.AddSingleton(serviceProvider => new DetectorEvaluationSessionStore(
             detectorEvaluationRoot,
             serviceProvider.GetRequiredService<TimeProvider>()));
