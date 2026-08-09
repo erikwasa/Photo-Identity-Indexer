45b8501f7da768db7552459fa3d81a03962089cc
Erik Wahlström <54457926+erikwasa@users.noreply.github.com>
2026-08-10 01:19:40 +0200

    Map orthogonal archive item filters

--- src/PhotoIdentity.Api/Program.cs
+++ src/PhotoIdentity.Api/Program.cs
@@ -132,6 +132,7 @@ public partial class Program
         app.MapDetectorEvaluationComparisonEndpoints();
         app.MapDetectorRolloutEndpoints();
         app.MapArchiveEndpoints();
+        app.MapArchiveItemFilterEndpoints();
         app.MapArchiveStorageEndpoints();
         app.MapFallbackToFile("index.html");
 
